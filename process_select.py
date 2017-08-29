1e4df3dd4cc2b6983a701f410e1667b352512281
aidatorajiro <kawarusosu@zoho.com>
2017-08-29 18:45:34 +0900

    change style / add fallback

--- process_select.py
+++ process_select.py
@@ -2,37 +2,24 @@
 
 from functools import partial
 from memorpy import MemWorker, Process
-from kivy.properties import ReferenceListProperty, ListProperty, NumericProperty, ObjectProperty, BooleanProperty, StringProperty
+from kivy.properties import (ReferenceListProperty,
+                             ListProperty,
+                             NumericProperty,
+                             ObjectProperty,
+                             BooleanProperty,
+                             StringProperty)
 from kivy.uix.widget import Widget
 from kivy.core.text import Label as CoreLabel
 from kivy.graphics import Rectangle, Color
+from kivy.graphics.instructions import InstructionGroup
 from kivy.animation import Animation
 from kivy.clock import Clock
 from utils import draw_text_on_canvas
+import os
 
 class ProcessSelect(Widget):
     current_process_index = NumericProperty(-1)
-    time_left = NumericProperty(1) # time left until the character dives
     process_list = ListProperty([])
-
-    overwrap = ObjectProperty(None)
-    overwrap_pos_x = NumericProperty(0)
-    overwrap_pos_y = NumericProperty(0)
-    overwrap_pos = ReferenceListProperty(overwrap_pos_x, overwrap_pos_y)
-    overwrap_size_w = NumericProperty(0)
-    overwrap_size_h = NumericProperty(66)
-    overwrap_size = ReferenceListProperty(overwrap_size_w, overwrap_size_h)
-    overwrap_r = NumericProperty(1)
-    overwrap_g = NumericProperty(1)
-    overwrap_b = NumericProperty(1)
-    overwrap_color = ReferenceListProperty(overwrap_r, overwrap_g, overwrap_b)
-    overwrap_color_anim = ObjectProperty(None)
-
-    text = ObjectProperty(None)
-    text_pos_x = NumericProperty(0)
-    text_pos_y = NumericProperty(0)
-    text_pos = ReferenceListProperty(text_pos_x, text_pos_y)
-    text_content = StringProperty("")
 
     dive_clock = ObjectProperty(None)
     finished = BooleanProperty(False)
@@ -40,6 +27,44 @@ class ProcessSelect(Widget):
     columns = 20
     cell_interval = 120
     cell_size = (64, 64)
+
+    # instruction groups
+    cells = ObjectProperty(None)
+    overwrap = ObjectProperty(None)
+    lebels = ObjectProperty(None)
+
+    # properties about overwrap rectangle
+    overwrap_pos_x = NumericProperty(0)
+    overwrap_pos_y = NumericProperty(0)
+    overwrap_pos = ReferenceListProperty(overwrap_pos_x, overwrap_pos_y)
+    overwrap_size_w = NumericProperty(0)
+    overwrap_size_h = NumericProperty(62)
+    overwrap_size = ReferenceListProperty(overwrap_size_w, overwrap_size_h)
+    overwrap_r = NumericProperty(0.03)
+    overwrap_g = NumericProperty(0.8)
+    overwrap_b = NumericProperty(1)
+    overwrap_a = NumericProperty(0)
+    overwrap_color = ReferenceListProperty(overwrap_r, overwrap_g, overwrap_b, overwrap_a)
+    overwrap_color_anim = ObjectProperty(None)
+
+    def index_to_pos(self, i):
+        """Calculate position from index"""
+        posx = (i % self.columns)*self.cell_interval
+        posy = int(i / self.columns)*self.cell_interval
+        return posx, posy
+
+    def pos_to_index(self, pos):
+        i = int(pos[0] / self.cell_interval) + int(pos[1] / self.cell_interval) * 20
+
+        if (pos[0] < 0 or
+                pos[1] < 0 or
+                pos[0] > self.columns*self.cell_interval or
+                pos[0] % self.cell_interval > self.cell_size[0] or
+                pos[1] % self.cell_interval > self.cell_size[1] or
+                i >= len(self.process_list)):
+            i = -1 # case of no select or invalid select
+
+        return i
 
     def start(self):
         """Initialize function."""
@@ -50,23 +75,35 @@ class ProcessSelect(Widget):
             message("エラーが発生しました。管理者権限でこのゲームを実行してみてください。")
             return
 
-        with self.canvas:
-            i = 0
+        self.cells = InstructionGroup()
+        self.overwrap = InstructionGroup()
+        self.lebels = InstructionGroup()
 
-            for proc in self.process_list:
-                # calculate position
-                posx = (i % self.columns)*self.cell_interval
-                posy = int(i / self.columns)*self.cell_interval
+        i = 0
 
-                # draw squares
-                Color(1, 1, 1)
-                Rectangle(pos=(posx, posy), size=self.cell_size)
+        pid_of_this_program = os.getpid()
 
-                # draw text
-                Color(0, 0, 0)
-                draw_text_on_canvas(str(proc["pid"]), pos=(posx, posy))
+        for proc in self.process_list:
+            # calculate position
+            posx, posy = self.index_to_pos(i)
 
-                i += 1
+            # draw squares
+            if proc["pid"] == pid_of_this_program:
+                self.cells.add(Color(0.8, 0.8, 0.8))
+            else:
+                self.cells.add(Color(1, 1, 1))
+            
+            self.cells.add(Rectangle(pos=(posx, posy), size=self.cell_size))
+
+            # draw text
+            self.lebels.add(Color(0, 0, 0))
+            self.lebels.add(draw_text_on_canvas(str(proc["pid"]), pos=(posx, posy)))
+
+            i += 1
+        
+        self.canvas.add(self.cells)
+        self.canvas.add(self.overwrap)
+        self.canvas.add(self.lebels)
 
     def update(self, _, chara):
         """Frame update function.
@@ -79,14 +116,7 @@ class ProcessSelect(Widget):
             return
 
         # calculate the index of selected process
-        i = int(coord[0] / self.cell_interval) + int(coord[1] / self.cell_interval) * 20
-        if (coord[0] < 0 or
-                coord[1] < 0 or
-                coord[0] > self.columns*self.cell_interval or
-                coord[0] % self.cell_interval > self.cell_size[0] or
-                coord[1] % self.cell_interval > self.cell_size[1] or
-                i >= len(self.process_list)):
-            i = -1 # case of no select or invalid select
+        i = self.pos_to_index(coord)
 
         # if selected process changed
         if i != self.current_process_index:
@@ -95,57 +125,40 @@ class ProcessSelect(Widget):
 
             # progress rectangle
             if i != -1:
-                pid = self.process_list[i]["pid"]
-
                 self.overwrap_color_anim = Animation(
-                    overwrap_r=0.97,
-                    overwrap_g=0.2,
-                    overwrap_b=0,
-                    overwrap_size_w=66,
+                    overwrap_a=1,
+                    overwrap_size_w=62,
                     duration=2.)
 
-                self.dive_clock = Clock.schedule_once(partial(self.dive, pid), 2.)
+                self.dive_clock = Clock.schedule_once(partial(self.dive, i), 2.)
 
                 self.overwrap_color_anim.start(self)
 
-                posx = (i % self.columns)*self.cell_interval
-                posy = int(i / self.columns)*self.cell_interval
+                posx, posy = self.index_to_pos(i)
 
-                self.overwrap_pos_x = posx-1
-                self.overwrap_pos_y = posy-1
-
-                self.text_content = str(pid)
-                self.text_pos_x = posx
-                self.text_pos_y = posy
+                self.overwrap_pos_x = posx+1
+                self.overwrap_pos_y = posy+1
             else:
-                self.overwrap_r = 1
-                self.overwrap_g = 1
-                self.overwrap_b = 1
+                self.overwrap_a = 0
                 self.overwrap_size_w = 0
-
                 self.overwrap_color_anim.stop(self)
-
                 self.dive_clock.cancel()
-        else:
-            self.time_left -= 0.01
 
-        # draw progress rectangle
-        if self.overwrap is not None:
-            self.canvas.remove(self.overwrap)
-            self.canvas.remove(self.text)
+        # draw overwrap
+        self.overwrap.clear()
+        self.overwrap.add(Color(*self.overwrap_color))
+        self.overwrap.add(Rectangle(pos=self.overwrap_pos, size=self.overwrap_size))
 
-        with self.canvas:
-            Color(*self.overwrap_color)
-            self.overwrap = Rectangle(pos=self.overwrap_pos, size=self.overwrap_size)
-            Color(0, 0, 0)
-            self.text = draw_text_on_canvas(self.text_content, pos=self.text_pos)
-
-    def dive(self, pid, _):
+    def dive(self, index, _):
         """Diving function."""
+        pid = self.process_list[index]["pid"]
         try:
             mw = MemWorker(pid=pid)
             self.finished = True
-            anim = Animation(opacity = 0)
+            anim = Animation(opacity=0)
             anim.start(self)
         except:
-            pass
+            posx, posy = self.index_to_pos(index)
+            with self.canvas:
+                Color(0.97, 0.1, 0)
+                draw_text_on_canvas("ERROR", font_size=20, pos=(posx+1, posy+30))
--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 from kivy.core.text import Label as CoreLabel
 from kivy.graphics import Rectangle
 
-def draw_text_on_canvas(arg_text, **kwargs):
+def draw_text_on_canvas(arg_text, font_size=12, **kwargs):
     # get text texture
-    label = CoreLabel(text=arg_text)
+    label = CoreLabel(text=arg_text, font_size=font_size)
     label.refresh()
-    tex   = label.texture
+    tex = label.texture
 
     # draw text
     return Rectangle(size=tex.size, texture=tex, **kwargs)
